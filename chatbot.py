b23cbcbc4c94287613d114fce16bc877cdda13bc
shreyavala <sxv180047@utdallas.edu>
2023-03-22 16:28:28 -0500

    test commit

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+# Portfolio 7: Chatbot
+# Names: Shreya Valaboju, Soham Mukherjee
+
+# Description
+
+#import libraries
+import os
+import nltk
+from nltk.chat.util import Chat, reflections
+import re
+import math
+import pickle
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+
+def chat():
+    print("Hello! My name is Champ, you can ask me any anything about the current players on the Dallas Mavs!")
+    #chat = Chat(pairs, reflections)
+    #chat.converse()
+
+
+
+
+
+if __name__ == '__main__':
+    chat()
+
+
